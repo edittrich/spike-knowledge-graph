3008d41fe285fb7066e4a7aabfbafbed17f3333d
Erik Dittrich <erik.dittrich@gmail.com>
2026-01-18 18:16:15 +0100

    Initial

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 
 async def main():
-    print(f"Connecting to Neo4j")
+    print(f"Connecting to Neo4j and Deleting Graph")
     password = os.getenv("NEO4J_PASSWORD")
     graph = Neo4jGraph(
         url="neo4j://127.0.0.1:7687",
@@ -22,29 +22,29 @@ async def main():
         refresh_schema=False,
     )
     graph.query("MATCH (n) DETACH DELETE n")
-    print(f"Connected to Neo4j\n")
+    print(f"Connected to Neo4j and Deleted Graph\n")
 
     print(f"Defining LLM and Graph Transformer")
     # llm = ChatGoogleGenerativeAI(temperature=0, model='gemini-2.5-flash')
     # llm = ChatOpenAI(temperature=0, model='gpt-4o')
-    # llm = ChatOllama(temperature=0, model="nemotron-3-nano:30b")
-    llm = ChatOllama(temperature=0, model="gpt-oss:20b")
+    llm = ChatOllama(temperature=0, model="nemotron-3-nano:30b")
+    # llm = ChatOllama(temperature=0, model="gpt-oss:20b")
 
     graph_transformer = LLMGraphTransformer(llm=llm)
     print(f"Defined LLM and Graph Transformer: \n{llm}\n")
 
-    print(f"Loading document")
+    print(f"Loading Document")
     loader = TextLoader("marie_curie_summary.txt")
     documents = loader.load()
-    print(f"Loaded document: \n{documents}\n")
+    print(f"Loaded Document: \n{documents}\n")
 
-    print(f"Creating graph")
+    print(f"Creating Graph")
     graph_documents = await graph_transformer.aconvert_to_graph_documents(documents)
-    print(f"Created graph: \n{graph_documents}\n")
+    print(f"Created Graph: \n{graph_documents}\n")
 
-    print(f"Adding graph")
+    print(f"Adding Graph")
     graph.add_graph_documents(graph_documents)
-    print(f"Added graph \n")
+    print(f"Added Graph \n")
 
 
 if __name__ == "__main__":
